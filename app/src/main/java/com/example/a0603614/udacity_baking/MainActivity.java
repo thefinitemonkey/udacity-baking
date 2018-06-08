c8d6964ecc26e34642ba1f3e40608927398006e7
Doug Brown <doug.brown@academy.com>
2018-06-08 16:01:17 -0500

    feat: Initial project setup. Add dependencies for Okio, OkHTTP, Moshi, Butterknife and Picasso.

---
+++ app/src/main/java/com/example/a0603614/udacity_baking/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a0603614.udacity_baking;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
